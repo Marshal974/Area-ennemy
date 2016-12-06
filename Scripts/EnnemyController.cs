d03d4fd3ee9b289e24a4dfaf82c26f4108aa1e5f
exopole <alexandre.nicaise28@gmail.com>
2016-12-06 01:02:07 +0100

    On branch master  Your branch is up-to-date with 'origin/master'.
    Premier commit : la detection d'ennemi dans la zone de detection
    fonctionne correctement. De plus, l'ennemi ciblera la cible la plus
    proche et passera a la suivante si celle-ci sort du perimettre ou meurt
    
    Fichiers interrassant : EnnemyController, ZoneController
    Fichiers utiles a ne pas oublier : UtilsDistance et UtilsPlayers. Pour
    l'instant ils ne sont pas tres fournis mais ils permettront de facilite
    la vie.
    
     Changes to be committed:
    	new file:   Scripts/CameraController.cs
    	new file:   Scripts/CameraController.cs.meta
    	new file:   Scripts/EnnemyController.cs
    	new file:   Scripts/EnnemyController.cs.meta
    	new file:   Scripts/PlayerController.cs
    	new file:   Scripts/PlayerController.cs.meta
    	new file:   Scripts/Rotator.cs
    	new file:   Scripts/Rotator.cs.meta
    	new file:   Scripts/UtilsDistance.cs
    	new file:   Scripts/UtilsDistance.cs.meta
    	new file:   Scripts/UtilsPlayers.cs
    	new file:   Scripts/UtilsPlayers.cs.meta
    	new file:   Scripts/ZoneController.cs
    	new file:   Scripts/ZoneController.cs.meta

---
+++ Scripts/EnnemyController.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+
+/***
+ * 
+ * \auteur : Exopole
+ * \Date : 03/12/2016
+ * 
+ * Cette classe permet de controller l'ennemi. Ca cible, son mouvement vers la cible et ca position d'origine ainsi que ca rencontre avec un joueur vivant.
+ * 
+ */
+public class EnnemyController : MonoBehaviour {
+    public float speed;
+
+    private GameObject target;
+
+    private Vector3 posInitial;
+    
+    private void Start()
+    {
+        posInitial = transform.position;
+    }
+
+    void FixedUpdate()
+    {
+
+        if (target != null)
+            goToPosition(target.transform.position);
+        else
+            goToPosition(posInitial);
+
+    }
+
+ 
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            if (UtilsPlayers.isAlive(collision.gameObject))
+            {
+                UtilsPlayers.setAlive(collision.gameObject, false);
+
+                if (collision.gameObject.Equals(target))
+                {
+                    target = null;
+                }
+            }
+            
+            
+
+        }
+    }
+
+    public void setTarget(GameObject target)
+    {
+        this.target = target;
+    }
+    
+
+    public void goToPosition(Vector3 destination)
+    {
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, destination, step);
+    }
+
+
+
+
+
+}
