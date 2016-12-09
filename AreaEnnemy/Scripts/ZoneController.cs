ab915b2439407a55c258f711d4763ca6cc34c699
exopole <alexandre.nicaise28@gmail.com>
2016-12-09 01:25:47 +0100

    Your branch is up-to-date with 'origin/master'. ajout de la zone de detection autour de l'ennemi  Changes to be committed: 	new file:   AreaEnnemy/Scripts/CameraController.cs 	new file:   AreaEnnemy/Scripts/CameraController.cs.meta 	new file:   AreaEnnemy/Scripts/EnnemyController.cs 	new file:   AreaEnnemy/Scripts/EnnemyController.cs.meta 	new file:   AreaEnnemy/Scripts/PlayerController.cs 	new file:   AreaEnnemy/Scripts/PlayerController.cs.meta 	new file:   AreaEnnemy/Scripts/Rotator.cs 	new file:   AreaEnnemy/Scripts/Rotator.cs.meta 	new file:   AreaEnnemy/Scripts/UtilsDistance.cs 	new file:   AreaEnnemy/Scripts/UtilsDistance.cs.meta 	new file:   AreaEnnemy/Scripts/UtilsPlayers.cs 	new file:   AreaEnnemy/Scripts/UtilsPlayers.cs.meta 	new file:   AreaEnnemy/Scripts/ZoneController.cs 	new file:   AreaEnnemy/Scripts/ZoneController.cs.meta 	new file:   VisionEnnemy/Scripts/PlayerMove.cs 	new file:   VisionEnnemy/Scripts/PlayerMove.cs.meta 	new file:   VisionEnnemy/Scripts/TargetDetection.cs 	new file:   VisionEnnemy/Scripts/TargetDetection.cs.meta

---
+++ AreaEnnemy/Scripts/ZoneController.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+/***
+ * 
+ * \auteur : Exopole
+ * \Date : 03/12/2016
+ * 
+ * Cette classe permet de controller la zone d'action d'un ennemie. Il peut cibler les ennemis à l'intérieur tant qu'ils sont vivants. 
+ * Il changeras de cible si une autres plus proches est présente ou si la cible est morte. Si aucune cible n'est présente il retournera à ca place d'origine
+ * 
+ */
+
+public class ZoneController : MonoBehaviour {
+    private GameObject targetPlayer;
+    [SerializeField] GameObject ennemy;
+
+    private string TAG = "ZoneController =>";
+
+
+    private void FixedUpdate()
+    {
+        if (targetPlayer != null && !UtilsPlayers.isAlive(targetPlayer))
+        {
+            targetPlayer = null;
+        }
+    }
+
+
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            setTargetMinDist(other.gameObject);
+        }
+    }
+
+
+
+    private void OnTriggerExit(Collider other)
+    {
+        
+        if (targetPlayer != null && targetPlayer.Equals(other.gameObject))
+        {
+            targetPlayer = null; // le joueur sort de la zone d'action de l'ennemi
+            setEnemyTargetPlayer(); // change la cible de l'ennemi
+            
+        }
+    }
+
+
+    private void setEnemyTargetPlayer()
+    {
+        ennemy.GetComponent<EnnemyController>().setTarget(targetPlayer);
+    }
+
+    // change pour la target la plus proche si celle ci est vivante
+    private void setTargetMinDist(GameObject player1)
+    {
+        if ( UtilsPlayers.isAlive(player1) // !! pour moi la vérification que players est non nul a été faite avant l'entré dans cette fonction
+            && (targetPlayer == null  
+                || ! UtilsPlayers.isAlive(targetPlayer) 
+                || ! UtilsDistance.isMinDist(this.ennemy, targetPlayer, player1)
+                )
+            )
+            {
+                this.targetPlayer = player1;
+                setEnemyTargetPlayer();
+            }
+    }
+   
+
+}
